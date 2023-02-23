831c18f2a750fed7d7ef3874b9923ff522a8466f
Davit Asryan <davit.asryan@nork.am>
2023-02-24 01:42:43 +0400

    project view

--- src/pages/projects/components/my-projects.tsx
+++ src/pages/projects/components/my-projects.tsx
@@ -1,14 +1,39 @@
-import { Col, PaginationProps, Row } from "antd"
-import useGetProjects from "api/projects/use-get-projects"
-import { CreateNewProjectButton, ProjectButton } from "components/button"
+import { Col, ColProps, PaginationProps, Row, RowProps } from "antd";
+import useGetProjects from "api/projects/use-get-projects";
+import { CreateNewProjectButton, ProjectButton } from "components/button";
 import { ProjectActionPopover } from "components/popover"
-import { TitleSeparator } from "components/typography"
-import { useSort } from "context/sort-context"
-import { useView, ViewTypes } from "context/view-context"
-import { useCallback, useEffect, useMemo, useReducer } from "react"
-import { FolderAction, folderReducer, initData } from "./folder/folders"
-import { ProjectActionContent } from "./project-action-content"
-import { ProjectActionTitle } from "./project-action-title"
+import { TitleSeparator } from "components/typography";
+import { useSort } from "context/sort-context";
+import { useView, ViewTypes } from "context/view-context";
+import { useCallback, useEffect, useMemo, useReducer } from "react";
+import { FolderAction, folderReducer, initData } from "./folder/folders";
+import { ProjectActionContent } from "./project-action-content";
+import { ProjectActionTitle } from "./project-action-title";
+
+let propsBlockView = {
+    row: {
+        gutter: 34,
+        justify: 'start'
+    },
+    col: {
+        span: 3
+    },
+    newProject: {},
+    project: {}, 
+    projectButton: {}, 
+};
+
+let propsGridView = {
+    row: {
+        gutter: [0, 16],
+    },
+    col: {
+        span: 24
+    },
+    newProject: { fullWidth: true, block: true },
+    project: { fullWidth: true }, 
+    projectButton: { fullWidth: true, block: true }, 
+};
 
 export const MyProjects = () => {
     const { state } = useView();
@@ -37,27 +62,20 @@ export const MyProjects = () => {
           total: data.length,
       }) : undefined, [data.length, onPaginationChange]);
 
+      const dataToDraw = useMemo(() => state === ViewTypes.Block ? propsBlockView : propsGridView, [state]);
+
     return <>
         <TitleSeparator name='All Projects' paginationProps={paginationProps} />
         {/* set if count < 7 start otherwise space-between  */}
-        {state === ViewTypes.Block ? <Row gutter={34} justify='start'>
-            <Col span={3}>
-                <CreateNewProjectButton />
+        <Row {...(dataToDraw.row as RowProps)}>
+            <Col {...(dataToDraw.col as ColProps)}>
+                <CreateNewProjectButton {...(dataToDraw.newProject)} />
             </Col>
-            <Col span={3}>
-                <ProjectActionPopover title={<ProjectActionTitle />} content={<ProjectActionContent />}>
-                    <ProjectButton project={{color: 'red', name: 'Knowledge Mode', type: 'public', dateTime: '2022.09.05 18:03'}} />
+            <Col {...(dataToDraw.col as ColProps)}>
+                <ProjectActionPopover title={<ProjectActionTitle />} content={<ProjectActionContent />} {...(dataToDraw.project)}>
+                    <ProjectButton project={{color: 'red', name: 'Knowledge Mode', type: 'public', dateTime: '2022.09.05 18:03'}} {...(dataToDraw.projectButton)} />
                 </ProjectActionPopover>
             </Col>
-        </Row> : <Row gutter={[0, 16]}>
-            <Col span={24}>
-                <CreateNewProjectButton fullWidth block />
-            </Col>
-            <Col span={24}>
-                <ProjectActionPopover fullWidth  title={<ProjectActionTitle />} content={<ProjectActionContent />}>
-                    <ProjectButton fullWidth block project={{color: 'red', name: 'Knowledge Mode', type: 'public', dateTime: '2022.09.05 18:03'}} />
-                </ProjectActionPopover>
-            </Col>
-        </Row>}
+        </Row>
     </>
 }
