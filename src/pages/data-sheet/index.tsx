234ca25a84f930b5ddc024962db98668a68aaf56
Davit Asryan <davit.asryan@nork.am>
2023-03-24 08:59:37 +0400

    fixed blicking of the screen

--- src/pages/data-sheet/index.tsx
+++ src/pages/data-sheet/index.tsx
@@ -1,36 +1,38 @@
-import { Col, Row as RowComponent } from "antd"
-import { useIsXXlScreen } from "hooks/use-breakpoint";
-import styled from "styled-components";
-import { LeftSection } from "./components/left-section";
-import { RightSection } from "./components/right-section";
-
+import { Col, Row as RowComponent } from 'antd';
+import styled from 'styled-components';
+import { LeftSection } from './components/left-section';
+import { RightSection } from './components/right-section';
 
 const Row = styled((props) => <RowComponent {...props} />)`
-    &.overview {
-        height: calc(100vh - 152px);
+  &.overview {
+    height: calc(100vh - 152px);
 
-        .ant-col.overview__section {
-            background: #F7F7F7;
+    .ant-col.overview__section {
+      background: #f7f7f7;
 
-            box-shadow: -10px 0px 10px rgba(111, 111, 111, 0.1);
+      box-shadow: -10px 0px 10px rgba(111, 111, 111, 0.1);
 
-            &:first-child {
-                opacity: 1;
-                box-shadow: inset -10px 10px 10px rgba(111, 111, 111, 0.1);
-            }
-        }
-
-        .overview-form-items {
-            min-height: 80vh;
-        }
+      &:first-child {
+        opacity: 1;
+        box-shadow: inset -10px 10px 10px rgba(111, 111, 111, 0.1);
+      }
     }
-    
+
+    .overview-form-items {
+      min-height: 80vh;
+    }
+  }
 `;
 
 export const DataSheet = () => {
-    const isXXL = useIsXXlScreen();
-    return <Row className="overview">
-    <Col span={isXXL ? 4 : 6} className='overview__section project-save'><LeftSection /></Col>
-    <Col span={isXXL ? 20 : 18} className='overview__section project-share'><RightSection /></Col>
-  </Row>
-}
+  return (
+    <Row className="overview">
+      <Col xs={6} xxl={4} className="overview__section project-save">
+        <LeftSection />
+      </Col>
+      <Col xs={18} xxl={20} className="overview__section project-share">
+        <RightSection />
+      </Col>
+    </Row>
+  );
+};
