0a7cf6f72b3cdd7aaabeef9d3c63862d78af6814
Davit Asryan <davit.asryan@nork.am>
2023-03-27 10:21:23 +0400

    sign in user

--- src/components/routes/public-route.tsx
+++ src/components/routes/public-route.tsx
@@ -1,46 +1,54 @@
 import { Col, Layout as LayoutComponent, Row } from 'antd';
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
 
 import styled from 'styled-components';
-import { COLORS } from 'helpers/constants';
+import { COLORS, PATHS } from 'helpers/constants';
 import { Logo } from 'components/logo';
+import { useAuth } from 'context/auth-context';
 
 const Layout = styled(LayoutComponent)`
-    background: #FDFDFD;
+  background: #fdfdfd;
 `;
 
 const { Header: HeaderComponent, Content: ContentComponent } = LayoutComponent;
 
 const Header = styled(HeaderComponent)`
-    && {
-        background: transparent;
-        padding: 32px 50px;
-        height: 107px;
-    }
+  && {
+    background: transparent;
+    padding: 32px 50px;
+    height: 107px;
+  }
 `;
 
 const Content = styled(ContentComponent)`
-    padding: 0 50px;
-    margin-left: auto;
-    margin-right: auto;
-    width: 448px;
+  padding: 0 50px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 448px;
 `;
 
 export const PublicRoutes = () => {
+  const { user } = useAuth();
+  // eslint-disable-next-line no-console
+  console.log('first', user);
+
+  if (user) {
+    return <Navigate to={PATHS.PROJECTS} />;
+  }
+
   return (
     <Layout>
       <Row>
         <Col span={12}>
-        <Header>
-          <Logo />
-        </Header>
-        <Content>
-          <Outlet />
-        </Content>
+          <Header>
+            <Logo />
+          </Header>
+          <Content>
+            <Outlet />
+          </Content>
         </Col>
         <Col span={12} style={{ backgroundColor: COLORS.PRIMARY.GRAY_LIGHT }}></Col>
       </Row>
-      
     </Layout>
-  )
+  );
 };
