274a93c5b4a1ff10d2cadfe06b37ebfabd091be5
Davit Asryan <davit.asryan@nork.am>
2023-03-30 16:40:17 +0400

    change vertical button place

--- src/components/button/vertical-button.tsx
+++ src/components/button/vertical-button.tsx
@@ -1,21 +1,34 @@
-import styled from 'styled-components';
-import { Button } from 'antd';
+import styled, { css } from 'styled-components';
+import { Button, ButtonProps } from 'antd';
 import { useTypeProperty } from 'pages/data-sheet/components/table-section/table-context';
 import { useCallback } from 'react';
 import { TypePropertyActionKind } from 'pages/data-sheet/components/table-section/types';
 import { PlusAction } from 'components/actions/plus';
 import { Text } from 'components/typography';
 import { COLORS } from 'helpers/constants';
+import { useDataSheetWrapper } from 'components/layouts/components/data-sheet/wrapper';
+import { useGetProjectNodeTypeProperties } from 'api/project-node-type-property/use-get-project-node-type-properties';
 
-export const Wrapper = styled(Button)`
-  /* transform: rotate(-90deg); */
-  height: 80%; //calculate rows count and height
+type WrapperProps = ButtonProps & {
+  rowsCount: number;
+};
+
+const LeftSidebarSize = 300;
+
+export const Wrapper = styled(({ rowsCount, ...props }: WrapperProps) => <Button {...props} />)`
+  height: 712px;
   width: 64px;
-  /* height: 64px;
-  width: 100%; */
   z-index: 4;
   position: absolute;
-  left: 200px; //variable
+  ${(props) =>
+    props.rowsCount * 200 > screen.width - LeftSidebarSize
+      ? css`
+      right 0;
+    `
+      : css`
+          left: ${props.rowsCount * 200}px;
+        `}
+
   background: linear-gradient(179.75deg, rgba(213, 215, 223, 0.9) 0%, rgba(213, 215, 223, 0.3) 99.91%);
   backdrop-filter: blur(2px);
   border: none;
@@ -41,6 +54,10 @@ export const Wrapper = styled(Button)`
 `;
 
 export const VerticalButton = () => {
+  const { nodeTypeId } = useDataSheetWrapper();
+  const { data } = useGetProjectNodeTypeProperties(nodeTypeId, { enabled: !!nodeTypeId });
+  // eslint-disable-next-line no-console
+  console.log('first', data, screen.width);
   const {
     dispatch,
     state: { addTypeisOpened },
@@ -50,7 +67,7 @@ export const VerticalButton = () => {
   }, [dispatch]);
 
   return !addTypeisOpened ? (
-    <Wrapper onClick={handlePropertyAddClick}>
+    <Wrapper onClick={handlePropertyAddClick} rowsCount={data?.length || 1}>
       <PlusAction />
       <Text className="property-text" color={COLORS.PRIMARY.BLUE}>
         Add Property
