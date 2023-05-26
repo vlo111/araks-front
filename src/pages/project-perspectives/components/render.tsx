31a8e7bf6750d642271bcb3c67b984601024fe46
vlo111 <vvcsharp@gmail.com>
2023-05-26 19:22:54 +0400

    spinning in perspective

--- src/pages/project-perspectives/components/render.tsx
+++ src/pages/project-perspectives/components/render.tsx
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 import { useGraphRef } from 'hooks/use-graph';
 import { useNodes } from 'hooks/use-nodes';
+import { Spin } from "antd";
 
 const Graph = styled.div`
   position: fixed;
   top: 152px;
   z-index: 0;
 `;
+const Spinner = styled(Spin)`
+  .ant-spin-dot {
+    position: fixed !important;
+  }
+`
 
 export const RenderSchema = () => {
   const ref = useGraphRef();
 
-  useNodes();
+   const { isInitialLoading } = useNodes();
 
-  return <Graph ref={ref} />;
+  return <Spinner spinning={isInitialLoading}> <Graph ref={ref} /></Spinner>;
 };
