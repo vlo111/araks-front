9bcf8ad3ec6392d7c13d072666ef7b3185b5da53
Davit Asryan <davit.asryan@nork.am>
2023-06-10 14:55:35 +0400

    return console.log

--- src/components/form/type/connection-type.tsx
+++ src/components/form/type/connection-type.tsx
@@ -31,6 +31,8 @@ const WrapperConnection = styled(Space)`
 
 export const ConnectionType = ({ data }: Props) => {
   const { dataList } = useDataSheetWrapper();
+  // eslint-disable-next-line no-console
+  console.log('data', data, dataList);
   const label = (
     <Space style={{ width: '100%', justifyContent: 'space-between', display: 'flex' }}>
       <Space>
