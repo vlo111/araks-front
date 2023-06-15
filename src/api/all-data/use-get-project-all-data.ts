6e35cd930bab199e60bee5b673f8b509b5cdb5b6
Davit Asryan <davit.asryan@nork.am>
2023-06-15 13:47:13 +0400

    get project all data

---
+++ src/api/all-data/use-get-project-all-data.ts
@@ -0,0 +1,36 @@
+import { useQuery, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import { PageParameters } from 'api/types';
+import { useParams } from 'react-router-dom';
+import { NodeDataListResponse, NodeDataResponse } from 'types/node';
+import client from '../client';
+import { URL_GET_PROJECT_ALL_DATA } from './constants';
+
+export const GET_PROJECT_EDGE_TYPE_PROPERTIES_LIST = '/projects-edge-type/:edge_type_id/properties';
+
+type ReturnData = {
+  data: NodeDataListResponse;
+};
+
+type Options = UseQueryOptions<ReturnData, Error, NodeDataListResponse>;
+type Result = UseQueryResult<NodeDataListResponse> & {
+  rowsData: NodeDataResponse[];
+  count: number;
+};
+
+export const useGetProjectAllData = (queryParams: PageParameters, options?: Options): Result => {
+  const params = useParams();
+  const urlNodes = URL_GET_PROJECT_ALL_DATA.replace(':project_id', params.id || '');
+  const result = useQuery({
+    queryKey: [urlNodes, queryParams],
+    queryFn: () => client.get(urlNodes, { params: queryParams }).then((data) => data.data),
+    ...options,
+  });
+  const { data, isSuccess } = result;
+
+  return {
+    ...result,
+    data: (isSuccess ? data : {}) as NodeDataListResponse,
+    rowsData: (isSuccess ? data.rows : []) as NodeDataResponse[],
+    count: (isSuccess ? data.count : 0) as number,
+  } as Result;
+};
