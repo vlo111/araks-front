e8118384fc049319280705f78f50c5b4954a5f30
Davit Asryan <davit.asryan@nork.am>
2023-06-19 17:25:37 +0400

    select list item checkbox

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,6 +1,12 @@
-import { Checkbox as CheckboxComponent } from "antd";
-import styled from "styled-components";
+import { Checkbox as CheckboxComponent } from 'antd';
+import styled from 'styled-components';
 
 export const Checkbox = styled(CheckboxComponent)`
-    
+  &.all-data-checkbox {
+    .ant-checkbox-input,
+    .ant-checkbox-inner {
+      width: 24px;
+      height: 24px;
+    }
+  }
 `;
--- src/pages/data-sheet/components/all-data/list.tsx
+++ src/pages/data-sheet/components/all-data/list.tsx
@@ -1,22 +1,56 @@
 import { Avatar, Col, List, Row } from 'antd';
 import { useGetProjectAllData } from 'api/all-data/use-get-project-all-data';
 import { PageParameters } from 'api/types';
+import { Checkbox } from 'components/checkbox';
 import { Text } from 'components/typography';
 import dayjs from 'dayjs';
 import { COLORS, DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE } from 'helpers/constants';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import { AllDataResponse } from 'types/node';
 
-const TypeInfo = styled.div`
+type TypeInfoProps = {
+  color?: string;
+};
+
+const TypeInfo = styled(({ color, ...props }) => <div {...props} />)<TypeInfoProps>`
   background-color: ${(props) => `${props.color}15`};
   text-align: center;
   border-radius: 8px;
+`;
+
+const StyledListItem = styled(({ color, ...props }) => <List.Item {...props} />)`
+  && {
+    margin-bottom: 8px;
+    padding-left: 24px;
+    padding-right: 32px;
+    background: linear-gradient(90.12deg, rgba(255, 255, 255, 0.7) 1.63%, rgba(255, 255, 255, 0.2) 100%);
+
+    &:hover {
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+
+      .all-data-type-name {
+        background-color: ${(props) => `${props.color}`};
+      }
+    }
+  }
 `;
 
 const initPageData: PageParameters = { page: DEFAULT_PAGE_NUMBER, size: DEFAULT_PAGE_SIZE };
 
 export const AllDataList = () => {
   const [pageData, setPageData] = useState(initPageData);
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+
+  const handleItemClick = useCallback(
+    (item: AllDataResponse) => {
+      const updatedCheckedItems = checkedItems.includes(item.id)
+        ? checkedItems.filter((i) => i !== item.id)
+        : [...checkedItems, item.id];
+      setCheckedItems(updatedCheckedItems);
+    },
+    [checkedItems]
+  );
 
   const { rowsData } = useGetProjectAllData(pageData);
   // eslint-disable-next-line no-console
@@ -41,7 +75,12 @@ export const AllDataList = () => {
         // eslint-disable-next-line no-console
         console.log('avatar, name', avatar, name);
         return (
-          <List.Item key={item.id}>
+          <StyledListItem key={item.id} color={name?.type_color} onClick={() => handleItemClick(item)}>
+            <Checkbox
+              className="all-data-checkbox"
+              style={{ marginRight: '16px' }}
+              checked={checkedItems.includes(item.id)}
+            />
             <List.Item.Meta
               avatar={<Avatar src={avatar?.node_name as string} />}
               title={
@@ -52,7 +91,7 @@ export const AllDataList = () => {
                   <Col span={18}>
                     <Row justify="end" align="middle" gutter={32}>
                       <Col offset={6} span={6}>
-                        <TypeInfo color={name?.type_color}>
+                        <TypeInfo color={name?.type_color} className="all-data-type-name">
                           <Text color={COLORS.PRIMARY.GRAY_DARK}>{name?.type_name}</Text>
                         </TypeInfo>
                       </Col>
@@ -66,7 +105,7 @@ export const AllDataList = () => {
                 </Row>
               }
             />
-          </List.Item>
+          </StyledListItem>
         );
       }}
     />
