4ef28ebb24d05d0f9f601baea5bba0260d250dbe
Davit Asryan <davit.asryan@nork.am>
2023-08-01 11:11:45 +0400

    Share section hide

--- src/pages/project-overview/components/share/shared-with.tsx
+++ src/pages/project-overview/components/share/shared-with.tsx
@@ -1,11 +1,15 @@
 import { Avatar, List } from 'antd';
+import { UserProjectRole } from 'api/types';
 import { Share } from 'components/dropdown';
 import { SHARE_OPTIONS } from 'components/dropdown/constants';
 import { Text } from 'components/typography';
 import { useAuth } from 'context/auth-context';
+import { useProject } from 'context/project-context';
 import { useIsXXlScreen } from 'hooks/use-breakpoint';
 
 export const SharedWith = () => {
+  const { projectInfo } = useProject();
+
   const isXXl = useIsXXlScreen();
   const { user: logedInUser } = useAuth();
   const data = [
@@ -20,7 +24,15 @@ export const SharedWith = () => {
       itemLayout={isXXl ? 'horizontal' : 'vertical'}
       dataSource={data}
       renderItem={(item, index) => (
-        <List.Item actions={[<Share defaultValue={item.value} key={item.title} />]}>
+        <List.Item
+          actions={[
+            projectInfo?.role === UserProjectRole.Owner ? (
+              <Share defaultValue={item.value} key={item.title} />
+            ) : (
+              SHARE_OPTIONS.find((option) => option.value === item.value)?.label
+            ),
+          ]}
+        >
           <List.Item.Meta avatar={<Avatar src={item.avatar} />} description={<Text>{item.title}</Text>} />
         </List.Item>
       )}
--- src/pages/project-overview/share.tsx
+++ src/pages/project-overview/share.tsx
@@ -1,14 +1,18 @@
 import { Col, Divider, Form, Row, Space } from 'antd';
+import { UserProjectRole } from 'api/types';
 import { Button } from 'components/button';
 import { FormItem } from 'components/form/form-item';
 import { Icon } from 'components/icon';
 import { Input } from 'components/input';
 import { VerticalSpace } from 'components/space/vertical-space';
 import { Text, Title } from 'components/typography';
+import { useProject } from 'context/project-context';
 import { COLORS } from 'helpers/constants';
 import { SharedWith } from './components/share/shared-with';
 
 export const Share = () => {
+  const { projectInfo } = useProject();
+
   const [form] = Form.useForm();
 
   const onFinish = () => {
@@ -23,42 +27,49 @@ export const Share = () => {
         </Title>
         <Form name="share" form={form} onFinish={onFinish} autoComplete="off" layout="vertical" requiredMark={false}>
           <VerticalSpace size={48}>
-            <VerticalSpace size={19}>
-              <Row>
-                <Col span={24}>
-                  <FormItem
-                    label="Perspectives"
-                    name="perspectives"
-                    rules={[{ required: true, message: 'Perspectives is required' }]}
-                    style={{ marginBottom: '0' }}
-                  >
-                    <Input placeholder="Main" />
-                  </FormItem>
-                </Col>
-              </Row>
-              <Row gutter={[37, 19]}>
-                <Col xs={24} xxl={16}>
-                  <FormItem
-                    name="email"
-                    rules={[{ required: true, message: 'Perspectives is required' }]}
-                    style={{ marginBottom: '0' }}
-                  >
-                    <Input placeholder="Email" />
-                  </FormItem>
-                </Col>
-                <Col xs={24} xxl={8}>
-                  <Button block type="primary">
-                    Send Invite
-                  </Button>
-                </Col>
-              </Row>
-            </VerticalSpace>
+            {projectInfo?.role === UserProjectRole.Owner && (
+              <VerticalSpace size={19}>
+                <Row>
+                  <Col span={24}>
+                    <FormItem
+                      label="Perspectives"
+                      name="perspectives"
+                      rules={[{ required: true, message: 'Perspectives is required' }]}
+                      style={{ marginBottom: '0' }}
+                    >
+                      <Input placeholder="Main" />
+                    </FormItem>
+                  </Col>
+                </Row>
+                <Row gutter={[37, 19]}>
+                  <Col xs={24} xxl={16}>
+                    <FormItem
+                      name="email"
+                      rules={[{ required: true, message: 'Perspectives is required' }]}
+                      style={{ marginBottom: '0' }}
+                    >
+                      <Input placeholder="Email" />
+                    </FormItem>
+                  </Col>
+                  <Col xs={24} xxl={8}>
+                    <Button block type="primary">
+                      Send Invite
+                    </Button>
+                  </Col>
+                </Row>
+              </VerticalSpace>
+            )}
             <VerticalSpace size={8}>
-              <Space size={9} style={{ lineHeight: 1 }}>
-                <Icon color="#C5C5C5" icon="public" size={20} />
-                <Text>Anyone with the link</Text>
-              </Space>
-              <Divider style={{ margin: '0', backgroundColor: '#C5C5C5' }} />
+              {projectInfo?.role === UserProjectRole.Owner && (
+                <>
+                  <Space size={9} style={{ lineHeight: 1 }}>
+                    <Icon color="#C5C5C5" icon="public" size={20} />
+                    <Text>Anyone with the link</Text>
+                  </Space>
+                  <Divider style={{ margin: '0', backgroundColor: '#C5C5C5' }} />
+                </>
+              )}
+              {projectInfo?.role === UserProjectRole.Editor && <Text>Project members</Text>}
               <SharedWith />
             </VerticalSpace>
           </VerticalSpace>
