aa9cba01efdb601b0b6a3bc7873d04b2227a384c
Davit Asryan <davit.asryan@nork.am>
2023-08-03 00:13:26 +0400

    hide property manipulation buttons for non wvers

--- src/pages/data-sheet/components/table-section/type-property/manage-connection-type-property.tsx
+++ src/pages/data-sheet/components/table-section/type-property/manage-connection-type-property.tsx
@@ -1,7 +1,9 @@
 import { PopoverProps } from 'antd';
+import { UserProjectRole } from 'api/types';
 import { ActionDots } from 'components/actions/dots';
 import { ConnectionTypePropertyMenu } from 'components/menu/connection-type-property-menu';
 import { ManageNodeTypePopover } from 'components/popover';
+import { useProject } from 'context/project-context';
 import React, { useState } from 'react';
 
 type Props = PopoverProps & {
@@ -12,27 +14,30 @@ type Props = PopoverProps & {
 
 export const ManageConnectionTypeProperty = React.memo(
   ({ children, propertyId, isDefault, canSetDefault, ...props }: Props) => {
+    const { projectInfo } = useProject();
     const [isManageOpened, setManageOpened] = useState(false);
 
     return (
       <>
         {children}
-        <ManageNodeTypePopover
-          content={
-            <ConnectionTypePropertyMenu propertyId={propertyId} closeManageNodes={() => setManageOpened(false)} />
-          }
-          open={isManageOpened}
-          trigger="click"
-          onOpenChange={(open: boolean) => {
-            !open && setManageOpened(false);
-            return open;
-          }}
-        >
-          <ActionDots
-            style={{ position: 'absolute', right: '5px', cursor: 'pointer', top: '30%' }}
-            onClick={() => setManageOpened(true)}
-          />
-        </ManageNodeTypePopover>
+        {projectInfo?.role === UserProjectRole.Owner && (
+          <ManageNodeTypePopover
+            content={
+              <ConnectionTypePropertyMenu propertyId={propertyId} closeManageNodes={() => setManageOpened(false)} />
+            }
+            open={isManageOpened}
+            trigger="click"
+            onOpenChange={(open: boolean) => {
+              !open && setManageOpened(false);
+              return open;
+            }}
+          >
+            <ActionDots
+              style={{ position: 'absolute', right: '5px', cursor: 'pointer', top: '30%' }}
+              onClick={() => setManageOpened(true)}
+            />
+          </ManageNodeTypePopover>
+        )}
       </>
     );
   }
--- src/pages/data-sheet/components/table-section/type-property/manage-type-property.tsx
+++ src/pages/data-sheet/components/table-section/type-property/manage-type-property.tsx
@@ -1,7 +1,9 @@
 import { PopoverProps } from 'antd';
+import { UserProjectRole } from 'api/types';
 import { ActionDots } from 'components/actions/dots';
 import { TypePropertyMenu } from 'components/menu/type-property-menu';
 import { ManageNodeTypePopover } from 'components/popover';
+import { useProject } from 'context/project-context';
 import React, { useState } from 'react';
 
 type Props = PopoverProps & {
@@ -12,32 +14,35 @@ type Props = PopoverProps & {
 
 export const ManageTypeProperty = React.memo(({ children, propertyId, isDefault, canSetDefault, ...props }: Props) => {
   const [isManageOpened, setManageOpened] = useState(false);
+  const { projectInfo } = useProject();
 
   return (
     <>
       {children}
-      <ManageNodeTypePopover
-        content={
-          <TypePropertyMenu
-            isDefault={isDefault}
-            propertyId={propertyId}
-            canSetDefault={canSetDefault}
-            closeManageNodes={() => setManageOpened(false)}
+      {projectInfo?.role === UserProjectRole.Owner && (
+        <ManageNodeTypePopover
+          content={
+            <TypePropertyMenu
+              isDefault={isDefault}
+              propertyId={propertyId}
+              canSetDefault={canSetDefault}
+              closeManageNodes={() => setManageOpened(false)}
+            />
+          }
+          open={isManageOpened}
+          trigger="click"
+          onOpenChange={(open: boolean) => {
+            !open && setManageOpened(false);
+            return open;
+          }}
+          {...props}
+        >
+          <ActionDots
+            style={{ position: 'absolute', right: '5px', top: '15px', cursor: 'pointer' }}
+            onClick={() => setManageOpened(true)}
           />
-        }
-        open={isManageOpened}
-        trigger="click"
-        onOpenChange={(open: boolean) => {
-          !open && setManageOpened(false);
-          return open;
-        }}
-        {...props}
-      >
-        <ActionDots
-          style={{ position: 'absolute', right: '5px', top: '15px', cursor: 'pointer' }}
-          onClick={() => setManageOpened(true)}
-        />
-      </ManageNodeTypePopover>
+        </ManageNodeTypePopover>
+      )}
     </>
   );
 });
