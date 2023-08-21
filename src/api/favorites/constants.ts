dcbfbad2272ddd168aa56a2d4c24bff01f5b48a6
Davit Asryan <davit.asryan@nork.am>
2023-08-21 12:35:16 +0400

    comment mentions

---
+++ src/api/favorites/constants.ts
@@ -0,0 +1,3 @@
+export const URL_FAVORIES_CREATE = '/favorites/favorite/:project_id';
+export const URL_FAVORIES_UPDATE = '/favorites/unfavorite/:project_id';
+export const URL_FAVORIES_LIST = '/favorites/:project_id';
---
+++ src/api/favorites/use-manage-comment.tsx
@@ -0,0 +1,23 @@
+import { ProjectCommentManage, RequestType, RequestTypes } from 'api/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import client from '../client';
+import { errorMessage } from 'helpers/utils';
+import { URL_FAVORIES_CREATE, URL_FAVORIES_LIST } from './constants';
+import { useParams } from 'react-router-dom';
+
+export const useManageComment = (baseUrl = URL_FAVORIES_CREATE, type: RequestType = RequestTypes.Post) => {
+  const params = useParams();
+  const url = baseUrl.replace(':project_id', params.id as string);
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (values: ProjectCommentManage) => {
+      return client[type](url, values);
+    },
+    onSuccess: (data, variables, context) => {
+      queryClient.invalidateQueries([URL_FAVORIES_LIST.replace(':project_id', params.id as string)]);
+    },
+    onError: errorMessage,
+  });
+  return mutation;
+};
