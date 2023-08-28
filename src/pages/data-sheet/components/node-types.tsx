e41ce00e292089c2a1e228f86f18a30b141f2f5e
Davit Asryan <davit.asryan@nork.am>
2023-08-28 14:42:13 +0400

    changed node type structure

--- src/pages/data-sheet/components/node-types.tsx
+++ src/pages/data-sheet/components/node-types.tsx
@@ -1,32 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Skeleton } from 'antd';
-import debounce from 'lodash.debounce';
-import { EventDataNode, TreeProps } from 'antd/es/tree';
+import { EventDataNode } from 'antd/es/tree';
 import { useDataSheetWrapper } from 'components/layouts/components/data-sheet/wrapper';
 import { PropsSetState, TableStyleBasedOnTab, TreeNodeType } from '../types';
 import { GET_PROJECT_NODE_TYPES_LIST, useGetProjectNoteTypes } from 'api/project-node-types/use-get-project-note-types';
 import { useParams } from 'react-router-dom';
 import { createNodesTree } from 'components/layouts/components/data-sheet/utils';
-import { useCallback, useEffect, useState } from 'react';
-import { SearchAction } from 'components/actions';
-import { filterTreeData } from '../utils';
-import { NodeTree } from 'components/tree/node-tree';
-import { ReactComponent as CaretDown } from 'components/icons/caret-down.svg';
-import { ReactComponent as CaretRight } from 'components/icons/caret-right.svg';
 import { DataSheetActionKind } from 'components/layouts/components/data-sheet/hooks/data-sheet-manage';
-
-const switcherIcon = ({ isLeaf, expanded }: { isLeaf: boolean; expanded: boolean }) => {
-  if (isLeaf) {
-    return null;
-  }
-  return expanded ? <CaretDown /> : <CaretRight />;
-};
+import { NodeTypesView } from './node-types-view';
+import { useCallback } from 'react';
 
 type Props = PropsSetState & TableStyleBasedOnTab;
 
 export const NodeTypes = ({ searchVisible, setSearchVisible, isCheckable = false, noColors = false }: Props) => {
   const params = useParams();
-  const [filteredData, setFilteredData] = useState<TreeNodeType[]>([]);
   const { selectNodeType, color, nodeTypeId, selectNodeTypeFinished, allTypeSelected, dispatch } =
     useDataSheetWrapper();
 
@@ -66,72 +52,43 @@ export const NodeTypes = ({ searchVisible, setSearchVisible, isCheckable = false
     noColors
   );
 
-  useEffect(() => {
-    if (nodesList && nodesList.length) {
-      setFilteredData(nodesList);
-    } else {
-      setFilteredData([]);
-    }
-  }, [nodesList]);
-
-  const onSelect = (selectedKeys: string[], e: { selected: boolean; node: EventDataNode<TreeNodeType> }) => {
-    selectNodeType({
-      titleText: e.node.name,
-      color: e.node.color,
-      nodeTypeId: e.node.id,
-      parentId: e.node.parent_id,
-      selectNodeTypeFinished: true,
-      nodesList,
-      dataList: data,
-    });
-  };
-
-  const onSearch = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const searchText = event.target.value.trim().toLowerCase();
-
-      debounce(() => {
-        const filteredData = filterTreeData(nodesList, searchText);
-        setFilteredData(filteredData);
-      }, 500)();
+  const onSelect = useCallback(
+    (selectedKeys: string[], e: { selected: boolean; node: EventDataNode<TreeNodeType> }) => {
+      selectNodeType({
+        titleText: e.node.name,
+        color: e.node.color,
+        nodeTypeId: e.node.id,
+        parentId: e.node.parent_id,
+        selectNodeTypeFinished: true,
+        nodesList,
+        dataList: data,
+      });
     },
-    [nodesList]
+    [data, nodesList, selectNodeType]
   );
 
-  const onClear = useCallback(() => {
-    setFilteredData(nodesList);
-  }, [nodesList]);
-
-  const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
-    dispatch({ type: DataSheetActionKind.ALL_DATA_TYPE_CHECK, payload: { allDataTypesList: checkedKeys as string[] } });
-  };
+  const onCheck = useCallback(
+    (checkedKeys: string[]) => {
+      dispatch({
+        type: DataSheetActionKind.ALL_DATA_TYPE_CHECK,
+        payload: { allDataTypesList: checkedKeys as string[] },
+      });
+    },
+    [dispatch]
+  );
 
   return !selectNodeTypeFinished || isInitialLoading ? (
     <Skeleton />
   ) : (
-    <>
-      {searchVisible && (
-        <SearchAction
-          isSearchActive={searchVisible}
-          onClear={onClear}
-          onChange={onSearch}
-          setSearchActive={setSearchVisible}
-        />
-      )}
-      <NodeTree
-        onSelect={onSelect}
-        showSearch
-        checkable={isCheckable}
-        onCheck={onCheck}
-        switcherIcon={switcherIcon}
-        selectedKeys={[nodeTypeId]}
-        defaultExpandedKeys={[nodeTypeId]}
-        treeData={filteredData}
-        autoExpandParent
-        blockNode
-        defaultExpandAll
-        color={color}
-      />
-    </>
+    <NodeTypesView
+      onSelect={onSelect}
+      onCheck={onCheck}
+      nodesList={nodesList}
+      searchVisible={searchVisible}
+      setSearchVisible={setSearchVisible}
+      isCheckable={isCheckable}
+      color={color}
+      nodeTypeId={nodeTypeId}
+    />
   );
 };
