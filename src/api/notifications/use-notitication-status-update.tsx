2e7aff2fe2bfc2755c30aa5ddc74f737eaeee74f
Davit Asryan <davit.asryan@nork.am>
2023-08-30 02:21:09 +0400

    file view and notification bug fix

--- src/api/notifications/use-notitication-status-update.tsx
+++ src/api/notifications/use-notitication-status-update.tsx
@@ -1,18 +1,15 @@
-import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 
 import client from '../client';
-import { URL_GET_NOTIFICATIONS_ALL_DATA, URL_UPDATE_NOTIFICATION_STATUS } from './constants';
+import { URL_UPDATE_NOTIFICATION_STATUS } from './constants';
 import { errorMessage } from 'helpers/utils';
 
 export const useNotificationStatusUpdate = (options?: UseMutationOptions<boolean, unknown, string>) => {
-  const queryClient = useQueryClient();
-
   const mutation = useMutation<boolean, unknown, string>({
     mutationFn: (notificationId: string) => {
       return client.put(URL_UPDATE_NOTIFICATION_STATUS.replace(':id', notificationId));
     },
     onSuccess: (data, variables, context) => {
-      queryClient.invalidateQueries([URL_GET_NOTIFICATIONS_ALL_DATA]);
       options?.onSuccess?.(data, variables, context);
     },
     onError: errorMessage,
