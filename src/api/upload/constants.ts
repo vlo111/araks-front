8b5da9015ef788142852534bfa9165959dc7338b
vlo111 <vvcsharp@gmail.com>
2023-09-01 19:45:03 +0400

    fix file-upload and image-upload

--- src/api/upload/constants.ts
+++ src/api/upload/constants.ts
@@ -1,3 +1,3 @@
-export const IMAGE_UPLOAD_URL = 'uploads/image-upload';
-export const FILE_UPLOAD_URL = 'uploads/file-upload';
+export const IMAGE_UPLOAD_URL = 'uploads/file-upload';
+export const FILE_UPLOAD_URL = 'uploads/image-upload';
 export const FILE_IMPORT_URL = 'uploads/excel-upload';
