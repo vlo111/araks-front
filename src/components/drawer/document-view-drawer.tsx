4b3a479162b25e08427d886647aa72cf5c89d182
Davit Asryan <davit.asryan@nork.am>
2023-09-04 20:09:16 +0400

    document view and search fixes

--- src/components/drawer/document-view-drawer.tsx
+++ src/components/drawer/document-view-drawer.tsx
@@ -1,56 +1,62 @@
-import { Drawer } from 'antd';
+import { Drawer, Space } from 'antd';
 import { Button } from 'components/button';
 import { Icon } from 'components/icon';
 import { Text } from 'components/typography';
-import { UploadedFileType } from 'types/node';
+import { NodeDataType, NodeDataTypes, UploadedFileType } from 'types/node';
 import { useState } from 'react';
 import { DocumentView } from 'components/document-view';
 import { CloseOutlined } from '@ant-design/icons';
 
 type Props = {
-  node: UploadedFileType;
+  items?: NodeDataTypes;
 };
 
-export const DocumentViewDrawer = ({ node }: Props) => {
-  const [openDrawer, setOpenDrawer] = useState(false);
+export const DocumentViewDrawer = ({ items }: Props) => {
+  const [openDrawer, setOpenDrawer] = useState<NodeDataType | undefined>();
 
   return (
     <>
-      <Button
-        type="link"
-        icon={<Icon icon="file1" size={20} color="red" />}
-        key={(node as UploadedFileType).url}
-        onClick={() => {
-          setOpenDrawer(true);
-        }}
-        style={{
-          display: 'flex',
-          height: '100%',
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '100%',
-          borderRadius: '2.324px',
-          background: 'linear-gradient(137deg, rgba(246, 246, 246, 0.80) 0%, rgba(246, 246, 246, 0.20) 100%)',
-          boxShadow: '0px 2.32421875px 3.486328125px 0px rgba(111, 111, 111, 0.10)',
-        }}
-      >
-        <Text style={{ maxWidth: '200px' }} ellipsis title={(node as UploadedFileType).name as string}>
-          {(node as UploadedFileType).name as string}
-        </Text>
-      </Button>
+      <Space>
+        {items?.map((node, index) => {
+          return (
+            <Button
+              key={index}
+              type="link"
+              icon={<Icon icon="file1" size={20} color="red" />}
+              onClick={() => {
+                setOpenDrawer(node);
+              }}
+              style={{
+                display: 'flex',
+                height: '100%',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%',
+                borderRadius: '2.324px',
+                background: 'linear-gradient(137deg, rgba(246, 246, 246, 0.80) 0%, rgba(246, 246, 246, 0.20) 100%)',
+                boxShadow: '0px 2.32421875px 3.486328125px 0px rgba(111, 111, 111, 0.10)',
+              }}
+            >
+              <Text style={{ maxWidth: '200px' }} ellipsis title={(node as UploadedFileType).name as string}>
+                {(node as UploadedFileType).name as string}
+              </Text>
+            </Button>
+          );
+        })}
+      </Space>
       <Drawer
-        open={openDrawer}
+        open={!!openDrawer}
         closable={false}
         destroyOnClose
         width={600}
-        getContainer={false}
+        getContainer={document.querySelector('.datasheet-view-drawer .ant-drawer-body') as HTMLElement}
         placement="right"
         drawerStyle={{
           background: '#F2F2F2',
           boxShadow: '10px 10px 10px 0px rgba(111, 111, 111, 0.10) inset',
         }}
         contentWrapperStyle={{
-          // marginTop: (document.querySelector('.datasheet-view-drawer .ant-drawer-header')?.clientHeight || 0) - 20,
+          marginTop: document.querySelector('.datasheet-view-drawer .ant-drawer-header')?.clientHeight || 0,
           boxShadow: 'none',
         }}
         style={{
@@ -62,10 +68,10 @@ export const DocumentViewDrawer = ({ node }: Props) => {
       >
         <div style={{ height: '100%', position: 'relative' }}>
           <CloseOutlined
-            onClick={() => setOpenDrawer(false)}
+            onClick={() => setOpenDrawer(undefined)}
             style={{ position: 'absolute', left: '-18px', top: '-18px' }}
           />
-          <DocumentView node={node} />
+          {openDrawer && <DocumentView node={openDrawer as UploadedFileType} />}
         </div>
       </Drawer>
     </>
--- src/pages/data-sheet/components/all-data/list-document.tsx
+++ src/pages/data-sheet/components/all-data/list-document.tsx
@@ -82,8 +82,8 @@ export const AllDataListDocument = ({ filterValue, setFilterValue, checkedItems,
                       </Button>
                     </Col>
                     <Col span={15}>
-                      {item.data?.map((dataItem) => (
-                        <Fragment key={dataItem.match_filename}>
+                      {item.data?.map((dataItem, index) => (
+                        <Fragment key={index}>
                           <Row justify="end" align="top" gutter={32}>
                             <Col xs={10} xxl={9}>
                               <Button
@@ -95,12 +95,12 @@ export const AllDataListDocument = ({ filterValue, setFilterValue, checkedItems,
                                 icon={<Icon color="red" icon="file1" size={16} />}
                               >
                                 <SecondaryText ellipsis>
-                                  <MarkedText longText={dataItem.match_filename} />
+                                  <MarkedText longText={dataItem.filename} />
                                 </SecondaryText>
                               </Button>
                             </Col>
                             <Col xs={11} xxl={12}>
-                              <MarkedText longText={dataItem.match_content} />
+                              <MarkedText longText={dataItem.match_filename || dataItem.match_content} />
                             </Col>
                             <Col xs={3} xxl={2}>
                               <Text color={COLORS.PRIMARY.GRAY}>({dataItem.match_count})</Text>
