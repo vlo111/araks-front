1f6dbd8e90572acb67249231667b11b5e6e66114
vlo111 <vvcsharp@gmail.com>
2023-09-07 17:00:30 +0400

    set context menu

--- src/components/layouts/components/visualisation/helpers/utils.tsx
+++ src/components/layouts/components/visualisation/helpers/utils.tsx
@@ -1,20 +1,24 @@
 import client from 'api/client';
 import { GetNeo4jData } from 'api/visualisation/use-get-data';
-import { ExpandList, ExpandListData } from '../types';
-import { allSvg, inSvg, outSvg } from './svgs';
+import { ExpandListData } from '../types';
 
 export const getExpandData = async (id: string, project_edge_type_id: string) => {
   const projectId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
 
   const url = `${process.env.REACT_APP_BASE_URL}neo4j/expand/${projectId}`;
 
-  const params = {
+  const params: { params: { id: string; direction?: string; project_edge_type_id?: string } } = {
     params: {
       id,
-      project_edge_type_id,
-      direction: 'all',
     },
   };
+
+  if (project_edge_type_id === 'all') {
+    params.params.direction = project_edge_type_id;
+  } else {
+    params.params.project_edge_type_id = project_edge_type_id;
+    params.params.direction = 'in';
+  }
 
   const data: GetNeo4jData = await client.get(url, params);
 
@@ -41,33 +45,11 @@ export const getExpandList = async (id: string) => {
  * @param isNode
  */
 export const getMenuContexts = (id: string, isNode: boolean) => {
-  const expandList: ExpandList = isNode ? JSON.parse(localStorage.getItem('node') ?? '') : [];
-
-  localStorage.removeItem('node');
-
-  const list = expandList.length
-    ? `${expandList
-        .map(
-          (l) =>
-            `<span id="${l.project_edge_type_id}"><p>${l.direction === 'in' ? inSvg : outSvg}</p> ${l.name} (${
-              l.count
-            })</span>`
-        )
-        .join()
-        .replaceAll(',', ' ')}`
-    : '';
-
-  const allCount = expandList.reduce((prev, acc) => acc.count + prev, 0);
-
-  const allData = `<span id="all"><p>${allSvg}</p> All (${allCount})</span>`;
-
   const nodeContext = `<div class='menu'>
       <span>Focus on node</span>
       <span class="main-menu expand">Expand</span>
       <div class='menu submenu-container'>
         <div class='submenu'>
-          ${allData}
-          ${list}
         </div>
       </div>
       <span class='delete'>Delete</span>
