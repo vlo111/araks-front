503a63d344e4a84fc9cc53e96b444104dc40e096
Gor <gor.ayvazyan.work@gmail.com>
2023-09-25 09:56:36 +0400

    Fix validation issues in edit and reset password

--- src/components/profile/edit/utils.tsx
+++ src/components/profile/edit/utils.tsx
@@ -1,27 +1,28 @@
+import { Rule } from 'antd/lib/form';
+
 const passwordRegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,16}$/;
 
-export const rulesPassword = [
+export const rulesPassword: Rule[] = [
   { required: true, message: 'The password is required' },
   { min: 9, message: 'The minimum length for this field is 9 characters' },
   { max: 16, message: 'The maximum length for this field is 16 characters' },
   {
     pattern: passwordRegExp,
-    message: 'Password must contain at least one uppercase character, one lowercase character and one digit ',
+    message: 'Password must contain at least one uppercase character, one lowercase character, and one digit',
   },
   ({ getFieldValue }: { getFieldValue: (name: string) => string }) => ({
-    async validator(_: { field: string }, value: string) {
-      if (
-        _.field === 'old_password' ||
-        getFieldValue(_.field === 'new_password' ? 'confirm_password' : 'new_password') === value
-      ) {
-        return await Promise.resolve();
+    async validator(_, value: string) {
+      const newPassword = getFieldValue('new_password');
+      const confirmNewPassword = getFieldValue('confirm_password');
+
+      if (newPassword && confirmNewPassword && newPassword !== confirmNewPassword) {
+        throw new Error('The two passwords that you entered do not match!');
       }
-      throw new Error('The two passwords that you entered do not match!');
     },
   }),
 ];
 
-export const rulesInput = [
+export const rulesInput: Rule[] = [
   { required: true, message: 'The field is required' },
   { min: 3, message: 'The minimum length for this field is 3 characters' },
   { max: 30, message: 'The maximum length for this field is 30 characters' },
