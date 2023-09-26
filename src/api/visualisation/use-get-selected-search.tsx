605184c03d4812b52fc0ab47f98e3b294647f75c
vlo111 <vvcsharp@gmail.com>
2023-09-26 16:40:15 +0400

    fix search node count

--- src/api/visualisation/use-get-selected-search.tsx
+++ src/api/visualisation/use-get-selected-search.tsx
@@ -7,7 +7,6 @@ import { Edges, Nodes } from './use-get-data';
 type ProjectResponse = {
   nodes: Nodes;
   edges: Edges;
-  count: number;
 };
 
 export const GET_SEARCH_DATA = '/neo4j/type-or-node/:project_id';
