bb3940ab3ea54b30d397c4e6a4214c414f31b435
vlo111 <vvcsharp@gmail.com>
2023-09-27 19:36:43 +0400

    fix node view

--- src/pages/project-visualisation/components/drawers/nodes/view-edit-node/node-view.tsx
+++ src/pages/project-visualisation/components/drawers/nodes/view-edit-node/node-view.tsx
@@ -1,33 +1,46 @@
 import { Text } from 'components/typography';
 import { VerticalSpace } from 'components/space/vertical-space';
-import { getRowData } from 'pages/data-sheet/components/table-section/node/utils';
-import { COLORS } from 'helpers/constants';
-import { FormInstance, Image } from 'antd';
-import React from 'react';
+import { getRowData, groupedData } from 'pages/data-sheet/components/table-section/node/utils';
+import { centerImageStyle, COLORS } from 'helpers/constants';
+import { Col, Image, Row } from 'antd';
+import React, { useMemo } from 'react';
 import { NodePropertiesValues } from 'types/node';
+import { Button, GridConnectionButton } from 'components/button';
+import { VIewDataType } from './index';
+import { ReactComponent as Connection } from 'components/icons/connection.svg';
+import { useGraph } from 'components/layouts/components/visualisation/wrapper';
 
 interface IProps {
-  formFata: FormInstance<{ name: string; icon: string }>;
   properties: NodePropertiesValues[] | undefined;
+  selectedView: VIewDataType;
 }
 
-export const ViewNode: React.FC<IProps> = ({ formFata, properties }) => {
-  const name = formFata.getFieldValue('name');
-  const img = formFata.getFieldValue('node_icon');
+export const ViewNode: React.FC<IProps> = ({ selectedView, properties }) => {
+  const { startOpenNode, finishOpenNode } = useGraph() ?? {};
+
+  const groupedDataList = useMemo(
+    () => (selectedView?.edges ? Object.entries(groupedData(selectedView.edges)) : []),
+    [selectedView?.edges]
+  );
 
   return (
     <VerticalSpace>
-      {img && img[0]?.response?.data?.uploadPath && (
-        <Image src={img[0]?.response?.data?.uploadPath} width={161} height={127} style={{ borderRadius: '4px' }} />
+      {selectedView?.default_image && (
+        <Image
+          src={selectedView?.default_image}
+          width={161}
+          height={127}
+          style={{ borderRadius: '4px', ...centerImageStyle }}
+        />
       )}
       <VerticalSpace>
         <Text color={COLORS.PRIMARY.BLUE}>name</Text>
-        <Text>{name && name[0]}</Text>
+        <Text>{selectedView?.name}</Text>
       </VerticalSpace>
       {properties ? (
-        properties.map((d) => {
+        properties.map((d, index) => {
           return (
-            <VerticalSpace key={d.id}>
+            <VerticalSpace key={d.id + index}>
               <div color={COLORS.PRIMARY.BLUE}>{d.nodeTypeProperty.name}</div>
               {getRowData(d)}
             </VerticalSpace>
@@ -36,6 +49,51 @@ export const ViewNode: React.FC<IProps> = ({ formFata, properties }) => {
       ) : (
         <></>
       )}
+      {groupedDataList.map(([index, item]) => (
+        <VerticalSpace key={index}>
+          <Row>
+            <Col>
+              <Text color={COLORS.PRIMARY.BLUE}>{item[0].edgeTypes.name}</Text>
+            </Col>
+            <Col span={8} offset={1}>
+              <GridConnectionButton
+                type="text"
+                size="small"
+                backgroundColor={item[0].nodes.nodeType.color}
+                block
+                icon={<Connection />}
+              >
+                {item[0].nodes.nodeType.name}
+              </GridConnectionButton>
+            </Col>
+            <Col
+              span={24}
+              style={{
+                gap: '0.5rem',
+                display: 'flex',
+                flexWrap: 'wrap',
+              }}
+            >
+              {item.map((row) => (
+                <Button
+                  type="text"
+                  key={row.id}
+                  onClick={() => {
+                    finishOpenNode();
+                    startOpenNode({
+                      id: row.target_id,
+                    });
+                  }}
+                >
+                  <Text underline color={COLORS.PRIMARY.GRAY_DARK}>
+                    {row.nodes.name}
+                  </Text>
+                </Button>
+              ))}
+            </Col>
+          </Row>
+        </VerticalSpace>
+      ))}
     </VerticalSpace>
   );
 };
