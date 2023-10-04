af0326d047df299dfa5153717b66036d824da0df
vlo111 <vvcsharp@gmail.com>
2023-10-04 11:35:30 +0400

    loading graph

--- src/pages/project-visualisation/components/graph/index.tsx
+++ src/pages/project-visualisation/components/graph/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { useGraphRef } from 'hooks/use-graph';
 import { Wrapper } from './wrapper';
 import { useGetData } from 'api/visualisation/use-get-data';
+import { Spin } from 'antd';
 
 export const Graph: React.FC = () => {
-  const { nodes, edges, count, relationsCounts } = useGetData();
+  const { nodes, edges, count, relationsCounts, isInitialLoading } = useGetData();
 
   const ref = useGraphRef({ nodes, edges, count, relationsCounts });
 
-  return <Wrapper ref={ref} />;
+  return (
+    <Spin style={{ maxHeight: '100%' }} spinning={isInitialLoading}>
+      <Wrapper ref={ref} />
+    </Spin>
+  );
 };
--- src/pages/project-visualisation/components/search/canvas/search.tsx
+++ src/pages/project-visualisation/components/search/canvas/search.tsx
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import { AutoComplete } from './auto-complete';
 import { SearchVisualization } from '../wrapper';
+import { Spin } from 'antd';
+import styled from 'styled-components';
+
+const Spinning = styled(Spin)`
+  position: fixed;
+  left: 0;
+  top: 150px;
+  width: 100% !important;
+  height: calc(100% - 150px) !important;
+  backdrop-filter: blur(6px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Search: React.FC = () => {
   const [stretchSearch, setStretchSearch] = useState(false);
@@ -16,6 +30,7 @@ export const Search: React.FC = () => {
 
   return (
     <>
+      {!!isEnterSearch && <Spinning spinning={true} />}
       <SearchVisualization className={`${stretchSearch ? 'stretch' : ''}`} handleSearch={handleSearch}>
         {stretchSearch && (
           <AutoComplete
