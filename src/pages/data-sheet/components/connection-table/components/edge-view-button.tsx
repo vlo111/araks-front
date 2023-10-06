64003d7042557f112204e945956752a0ace394a2
Davit Asryan <davit.asryan@nork.am>
2023-10-06 14:39:50 +0400

    prevent opening connection with viewer rowl

--- src/pages/data-sheet/components/connection-table/components/edge-view-button.tsx
+++ src/pages/data-sheet/components/connection-table/components/edge-view-button.tsx
@@ -1,6 +1,8 @@
+import { UserProjectRole } from 'api/types';
 import { Button } from 'components/button';
 import { MenuText } from 'components/typography';
 import { useViewDatasheetEdge } from 'context/datasheet-edge-view-vontext';
+import { useProject } from 'context/project-context';
 import { COLORS } from 'helpers/constants';
 import { useIsPublicPage } from 'hooks/use-is-public-page';
 import styled from 'styled-components';
@@ -23,10 +25,18 @@ const CedllName = styled(MenuText)`
 export const EdgeViewButton = ({ text, rowData }: Props) => {
   const { dispatch } = useViewDatasheetEdge();
   const isPublicPage = useIsPublicPage();
+  const { projectInfo } = useProject();
 
+  // eslint-disable-next-line no-console
+  console.log('projectInfo', projectInfo);
   return (
     <>
-      <Button type="link" className="table-row-height" onClick={() => dispatch(rowData)} disabled={isPublicPage}>
+      <Button
+        type="link"
+        className="table-row-height"
+        onClick={() => dispatch(rowData)}
+        disabled={isPublicPage || projectInfo?.role === UserProjectRole.Viewer}
+      >
         <CedllName underline color={COLORS.PRIMARY.BLUE}>
           {text}
         </CedllName>
