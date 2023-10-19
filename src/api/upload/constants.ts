4b57ced2929696f0c793480a50c876c84cfd50d7
vlo111 <vvcsharp@gmail.com>
2023-10-19 17:25:56 +0400

    fix avatar upload path

--- src/api/upload/constants.ts
+++ src/api/upload/constants.ts
@@ -1,4 +1,4 @@
 export const FILE_UPLOAD_URL = 'uploads/file-upload';
 export const IMAGE_UPLOAD_URL = 'uploads/image-upload';
 export const FILE_IMPORT_URL = 'uploads/excel-upload';
-export const AVATAR_UPLOAD_URL = 'uploads/excel-upload';
+export const AVATAR_UPLOAD_URL = 'uploads/avatar-upload';
