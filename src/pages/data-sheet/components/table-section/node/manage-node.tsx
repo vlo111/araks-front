b610fb43d487fb7d637c7daeaa0972c1c3bb8e58
vlo111 <vvcsharp@gmail.com>
2023-11-08 16:04:02 +0400

    add node create source data in data sheet

--- src/pages/data-sheet/components/table-section/node/manage-node.tsx
+++ src/pages/data-sheet/components/table-section/node/manage-node.tsx
@@ -69,6 +69,8 @@ export const ManageNode = ({ tableHead, tableHeight }: Props) => {
         const formName = getConnectionFormName(item.name, item.id);
         return item.ref_property_type_id === PropertyTypes.Connection
           ? (values[formName] as NodeDataConnectionToSave[])?.map((itemConn) => ({
+              source_id: itemConn.source_id,
+              source_type_id: itemConn.source_type_id,
               target_id: itemConn.target_id,
               target_type_id: itemConn.target_type_id,
               project_edge_type_id: itemConn.id,
