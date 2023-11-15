7b73eab8a61afff65cbd7fe0b1e4ecedb5b5d640
vlo111 <vvcsharp@gmail.com>
2023-11-15 15:52:56 +0400

    add enum queries and datasheet

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -128,7 +128,11 @@ type ItemType =
   | (NodeEdgeTypesReturnData & DefaultOptionType & { type: QueryFilterTypes; typeText: string });
 
 export const getQueryValue = (item: ItemType) => {
-  const { type, betweenStart, betweenEnd, typeText } = item;
+  const { type, betweenStart, betweenEnd, typeText, ref_property_type_id } = item;
+
+  if (ref_property_type_id === PropertyTypes.ENUM) {
+    return typeText;
+  }
 
   const isBeforeAfter = (item: ItemType) => {
     return [QueryFilterTypes.BEFORE, QueryFilterTypes.AFTER].includes(item.type);
